217b41ab64fd11c4181e78b93a19256f444a9d01
sushant kumar <kumarsushant3880@gmail.com>
2026-01-16 12:53:57 +0530

    session note

--- script.js
+++ script.js
@@ -13,9 +13,14 @@
 
 // Q2. find out the sum of all the elements inside an array using for loop.
 
-let set = [ 5,6,7,8,9,10]
-let sum = 0;
-for(let i = 0; i < set.length; i++){
-    sum += set[i];
-}
-console.log(sum);
+// let set = [ 5,6,7,8,9,10]
+// let sum = 0;
+// for(let i = 0; i < set.length; i++){
+//     sum += set[i];
+// }
+// console.log(sum);
+
+// Q3. diffrentiate btw map , reduce and filter.
+// map method returns same array.
+// filter method returns subset of array.
+// reduce method returns single value only.
