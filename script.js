d50e72345271008d2171d891219782a89d905614
sushant kumar <kumarsushant3880@gmail.com>
2026-01-16 12:43:33 +0530

    first change

---
+++ script.js
@@ -0,0 +1,21 @@
+// 1. regular function
+
+// function sample(a){
+//     console.log(a,"sample");
+// }
+// sample(5)
+
+// 2. arrow function
+// filter method
+// let marks = [4,552,47,5,17,5];
+// let result = marks.filter((mark)=>16);
+// console.log(result);
+
+// Q2. find out the sum of all the elements inside an array using for loop.
+
+let set = [ 5,6,7,8,9,10]
+let sum = 0;
+for(let i = 0; i < set.length; i++){
+    sum += set[i];
+}
+console.log(sum);
