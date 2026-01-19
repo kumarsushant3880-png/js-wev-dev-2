f354b4af67796fdf938bc5f4639e3ceea62de6d6
sushant kumar <kumarsushant3880@gmail.com>
2026-01-19 13:33:48 +0530

    day 3

---
+++ js-day-3/script3.js
@@ -0,0 +1,29 @@
+// push
+// let fruits = ["apple ","banana"];
+// let newlenght = fruits.push("cherry");
+// console.log(newlenght);
+// console.log(fruits);
+
+
+//pop
+// let fruits = ["apple", "banana","cherry"];
+// let removed = fruits.pop();
+// console.log(removed);
+// console.log(fruits);
+
+
+//shift
+// let fruits = ["apple","banana","cherry"];
+// let removed = fruits.shift();
+// console.log(removed);
+// console.log(fruits);
+
+
+
+//Q1. reverse the array using push and pop method.
+let arr = [1, 2, 3, 4, 5];
+let reversed = [];
+while (arr.length > 0) {
+    reversed.push(arr.pop());
+}
+console.log(reversed); 
