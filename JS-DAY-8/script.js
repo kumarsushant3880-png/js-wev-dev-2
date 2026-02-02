0d0c87d897ac0b2110230a39728b108909fca979
sushant kumar <kumarsushant3880@gmail.com>
2026-02-02 13:34:36 +0530

    session 8

--- JS-DAY-8/script.js
+++ JS-DAY-8/script.js
@@ -3,3 +3,10 @@ console.log(profile);
 profile.style.backgroundColor="#f5f5f5"
 profile.style.padding=["15px"]
 profile.style.textAlign= "center"
+
+const important = document.getElementsByClassName("important")
+console.log(important)
+for(let i=0;i<important.length;i++){
+    important[i].style.color="red";
+    important[i].style.fontSize=["60px"];
+}
